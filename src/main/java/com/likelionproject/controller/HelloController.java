98f7f6cea0bde2651d5c5153933ab48c0ce07e10
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-12-20 11:17:16 +0900

    remove spring-security dependency, GET /api/v1/hello

--- src/main/java/com/likelionproject/controller/HelloController.java
+++ src/main/java/com/likelionproject/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.likelionproject.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
