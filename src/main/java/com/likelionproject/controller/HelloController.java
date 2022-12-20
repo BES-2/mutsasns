0f3221bf9ef189f53bb21249fe34d7394a50bf08
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-12-20 11:09:59 +0900

    first commit

---
+++ src/main/java/com/likelionproject/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.likelionproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("hello");
+    }
+}
