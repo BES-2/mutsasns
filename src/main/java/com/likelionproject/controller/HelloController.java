3ddca07a5410dae24900ddbd7e25bd26b7e8f688
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-12-27 09:02:11 +0900

    [update]: 출석 keyword 변경. - happy_new_year > 한건주

--- src/main/java/com/likelionproject/controller/HelloController.java
+++ src/main/java/com/likelionproject/controller/HelloController.java
@@ -13,6 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok().body("happy_new_year");
+        return ResponseEntity.ok().body("한건주");
     }
 }
